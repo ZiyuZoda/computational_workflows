7a763f4e251a6451d4d586d1b235ece966d20560
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-07-25 23:09:09 -0400

    Create main.js

---
+++ excercise_06_1/main.js
@@ -0,0 +1,16 @@
+let btn= document.querySelector('button');
+
+function btnClickFn(){
+btn.classList.add('T-I-JW');
+
+
+}
+function btnClickFn(){
+  btn.classList.remove('T-I-JW');
+  
+  
+  }
+  
+
+btn.addEventListener('mousedown', btnDownFn)
+btn.addEventListener('mouseup', btnUpFn)
