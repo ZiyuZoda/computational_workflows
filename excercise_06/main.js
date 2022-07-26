e51fa4b3e24ad248f8ab5dbdbe6a4bf3dfc66a5d
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-07-25 23:09:20 -0400

    Create main.js

---
+++ excercise_06/main.js
@@ -0,0 +1,10 @@
+function keyAlertFn (event) {
+    console.log(event)
+  
+    let name = event.key;
+    let code = event.keyCode;
+    
+    alert(`This is the letter ${name} with a code of ${code}`)
+  }
+  
+  window.addEventListener('keydown', keyAlertFn);
