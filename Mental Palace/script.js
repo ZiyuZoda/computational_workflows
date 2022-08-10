82b65f97cf208310c7fcafacc9084f6030b9e3d9
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-08-10 01:40:19 -0400

    Update script.js

--- Mental Palace/script.js
+++ Mental Palace/script.js
@@ -1,14 +1,5 @@
 
 
-$(window).scroll(function(e) {
-
-  var elOffset = $('.element').scrollTop() - $(window).scrollTop();
-
-  if(elOffset < 450) {
-      $(this).addClass('blur');
-  }
-
-});
 
 
 
