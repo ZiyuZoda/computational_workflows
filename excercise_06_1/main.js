2fae8a3814b953a5e56a1842adf67c72941d9e7a
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-08-09 22:50:21 -0400

    Update main.js

--- excercise_06_1/main.js
+++ excercise_06_1/main.js
@@ -1,16 +1,33 @@
-let btn= document.querySelector('button');
+// let btn= document.querySelector('button');
 
-function btnClickFn(){
-btn.classList.add('T-I-JW');
+// function btnClickFn(){
+// btn.classList.add('T-I-JW');
 
 
-}
-function btnClickFn(){
-  btn.classList.remove('T-I-JW');
+// }
+// function btnClickFn(){
+//   btn.classList.remove('T-I-JW');
   
   
+//   }
+  
+
+// btn.addEventListener('mousedown', btnDownFn)
+// btn.addEventListener('mouseup', btnUpFn)
+
+
+
+function previewFile() {
+  const preview = document.querySelector('img');
+  const file = document.querySelector('input[type=file]').files[0];
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    // convert image file to base64 string
+    preview.src = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsDataURL(file);
   }
-  
-
-btn.addEventListener('mousedown', btnDownFn)
-btn.addEventListener('mouseup', btnUpFn)
+}
