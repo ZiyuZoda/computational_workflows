fcd84178b3a8b31ef8ec9c304b39ffa304a09e14
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-08-10 00:24:53 -0400

    Update script2.js

--- Mental Palace/script2.js
+++ Mental Palace/script2.js
@@ -1,3 +1,4 @@
+// read img and show it
 function previewFile() {
   const preview = document.querySelector('img');
   const file = document.querySelector('input[type=file]').files[0];
@@ -10,17 +11,25 @@ function previewFile() {
 
   if (file) {
     reader.readAsDataURL(file);
+    
   }
 }
+// when upload, dive into homepage
+let btn= document.querySelector('button');
 
+function btnClickFn(){
 
+location = "Homepage.html"
+window.alert("Thank you for sharing your treasure with us")
+}
 
+btn.addEventListener('mousedown', btnClickFn)
 
-
-
+//blob
 const tween = KUTE.fromTo(
   '#blob1',
   { path: '#blob1' },
   { path: '#blob2' },
   { repeat: 999, duration: 3000, yoyo: true }
 ).start();
+
