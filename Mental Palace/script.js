b824a4403531fb4ad4d1a2879ace1df583f95a69
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-08-10 20:31:45 -0400

    Update script.js

--- Mental Palace/script.js
+++ Mental Palace/script.js
@@ -1,4 +1,12 @@
+let btn= document.querySelector('button');
 
+function btnClickFn(){
+
+location = "index.html"
+// window.alert("Thank you for sharing your treasure with us")
+}
+
+btn.addEventListener('mousedown', btnClickFn)
 
 
 
