9e1f9947c8693a6105aea535fdafcb51162115d1
ZiyuZoda <109115725+ZiyuZoda@users.noreply.github.com>
2022-08-11 17:10:02 -0400

    Update script.js

--- Mental Palace/script.js
+++ Mental Palace/script.js
@@ -10,6 +10,13 @@ btn.addEventListener('mousedown', btnClickFn)
 
 
 
+// var dataImage = localStorage.getItem('imgData');
+// bannerImg = document.getElementById('tableBanner');
+// bannerImg.src = "data:image/png;base64," + dataImage;
 
-
-
+window.onload = function() {
+    var picture = localStorage.getItem('imgData');
+    var image = document.createElement('img');
+    image.src = picture;
+    document.body.appendChild(image);
+   };
